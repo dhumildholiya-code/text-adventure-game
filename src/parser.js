85ad1cefabb0588605eed241411c2dcce2dd627a
dhumildholiya-code <dhumildholiya97@gmail.com>
2023-06-07 22:47:49 +0530

    setup data table for vocabs for ast evalution.

--- src/parser.js
+++ src/parser.js
@@ -1,9 +1,9 @@
-const verbs = ["go", "pick", "attack"];
-const preposition = ["to","in","from","up","with","on"];
-const directions = ["north", "south", "east", "west"];
-const word = ["box", "wooden", "stick", "bag", "stone", "troll", "iron", "sword"];
-const gameObjects = ["iron sword", "wooden stick"];
-const clean = ["a", "an", "the"];
+const verbs = {"go":0, "pick":1, "attack":2};
+const preposition = {"to":0,"in":1,"from":2,"up":3,"with":4,"on":5};
+const directions = {"north":0, "south":1, "east":2, "west":3};
+const word = {"box":0, "wooden":1, "stick":2, "bag":3, "stone":4, "troll":5, "iron":6, "sword":7};
+const gameObjects = {"iron sword":0, "wooden stick":1, "wooden troll":2};
+const clean = {"a":0, "an":1, "the":2};
 const tokenPatterns = [
     {type: "verb", pattern: verbs},
     {type: "preposition", pattern: preposition},
@@ -11,14 +11,15 @@ const tokenPatterns = [
     {type:"word", pattern: word},
     {type:"null", pattern: clean},
 ];
-function Token(tokenType, literal){
+function Token(tokenType, literal, value){
     this.tokenType = tokenType;
     this.literal = literal;
+    this.value =value;
 }
 /**
  * Represents Node of AST(Abstract Syntax Tree) 
  * @param {String} type type of node
- * @param {String} value value of node
+ * @param {Number} value value of node
  * @param {Node} left left child of node
  * @param {Node} right right child of node
  */
@@ -39,16 +40,16 @@ function parseNext(tokens, prev){
     }
     if(prev == null && (tok.tokenType == "verb" || tok.tokenType == "word")){
         tokens.shift();
-        return parseNext(tokens, new TerminalNode(tok.tokenType, tok.literal));
+        return parseNext(tokens, new TerminalNode(tok.tokenType, tok.value));
     }
     else if(prev.type == "verb" && (tok.tokenType == "direction" || tok.tokenType == "word")){
         let right = parseNext(tokens, null);
-        return new Node("join", "", prev, right);
+        return new Node("join", 0, prev, right);
     }
     else if(tok.tokenType == "preposition"){
         tokens.shift();
         let right = parseNext(tokens, null);
-        return new Node(tok.tokenType, tok.literal, prev, right);
+        return new Node(tok.tokenType, tok.value, prev, right);
     }
     else{
         return prev;
@@ -62,8 +63,8 @@ function parseCommand(source){
     const tokensObj = tokenizer(source);
     if(tokensObj.err == ""){
         const tokens = tokensObj.tokens;
-        // const sentence = parseNext(tokens, null);
-        // console.log(sentence);
+        const sentence = parseNext(tokens, null);
+        console.log(sentence);
         tokens.forEach(element => {
             console.log(element);
         });
@@ -88,12 +89,13 @@ function tokenizer(source){
         let counter = 0;
         for(const item of tokenPatterns){
             counter++;
-            if(item.pattern.includes(word)){
+            let keys = Object.keys(item.pattern);
+            if(keys.includes(word)){
                 if(item.type == "null"){
                     counter = 0;
                     break;
                 }
-                tokens.push( new Token(item.type , word));
+                tokens.push( new Token(item.type , word, item.pattern[word]));
                 counter = 0;
                 break;
             }
@@ -107,19 +109,23 @@ function tokenizer(source){
         }
     }
     let combineWord = "";
+    let goKeys = Object.keys(gameObjects);
     for(let i=0;i<tokens.length - 1;i++){
         if(tokens[i].tokenType == "word" && tokens[i+1].tokenType =="word"){
             combineWord = `${tokens[i].literal} ${tokens[i+1].literal}`;
-            if(gameObjects.includes(combineWord)){
-                newTokens.push(new Token("word", combineWord));
+            if(goKeys.includes(combineWord)){
+                newTokens.push(new Token("word", combineWord, gameObjects[combineWord]));
             }
+        }
+        else if(tokens[i].tokenType == "word" && tokens[i+1].tokenType !="word"){
+            continue;
         }
         else{
             newTokens.push(tokens[i]);
             combineWord = "";
         }
     }
-    newTokens.push(new Token("EOF", null));
+    newTokens.push(new Token("EOF", null, 0));
     return ({
         tokens: newTokens,
         err: err,
@@ -127,4 +133,4 @@ function tokenizer(source){
 }
 
 // parseCommand("attack troll with he iron sword");
-parseCommand("attack on a troll with the iron sword");
+parseCommand("attack on a wooden troll with the iron sword");
